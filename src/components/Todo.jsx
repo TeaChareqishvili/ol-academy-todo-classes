30f98cf489dfa168cead7cd5fd3451b51885452f
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-05-21 22:53:32 +0400

    edit input

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./todoStyle.scss";
 
 class Todo extends React.Component {
   constructor(props) {
@@ -6,14 +7,28 @@ class Todo extends React.Component {
     this.state = {
       todos: [
         {
-          text: "Learn react",
+          text: "Learn React",
           id: 1,
+        },
+        {
+          text: "Learn Js",
+          id: 2,
+        },
+        {
+          text: "learn Scss",
+          id: 3,
         },
       ],
       inputValue: " ",
+      editInput: {
+        edit: false,
+        id: undefined,
+        inputValue: "",
+      },
     };
   }
 
+  // პირველად თუ დავაკლიკე ცარიელ ინფუთს ლისტში ამატებს ცარიელს და მერე აღარ. ????
   addTodo = () => {
     const usedId = this.state.todos.map((e) => e.id);
     const addId = Math.max(...usedId) + 1;
@@ -33,38 +48,87 @@ class Todo extends React.Component {
     this.setState({ todos: deleteTodosItems });
   };
 
+  editTodo = (id, inputValue) => {
+    this.setState({
+      editInput: {
+        id,
+        edit: true,
+        inputValue,
+      },
+    });
+  };
+
+  addEdit=()=>{
+    const todo = [...this.state.todos]
+    todo.map((e)=>{
+        if (e.id === this.state.editInput.id){
+            e.text = this.state.editInput.inputValue
+        }
+       
+    })
+    this.setState({todos:todo})
+    this.setState({
+        editInput:{
+           edit:false
+        }
+    })
+   
+   
+  }
+
   render() {
     return (
-      <div>
-        <h1>To do list</h1>
+      <div className="todoWrapper">
+        <div className="todo">
+          <h1>Todo list</h1>
 
-        {this.state.todos.length > 0 ? (
-          <div>
-            <ul>
-              {this.state.todos.map((todo) => (
-                <li key={todo.id}>
-                  <div>
-                    {todo.text}
+          {this.state.todos.length > 0 ? (
+            <div className="list">
+              <ul>
+                {this.state.todos.map((todo) => (
+                  <>
+                    <li key={todo.id}>{todo.text}</li>
                     <button onClick={() => this.deleteTodo(todo.id)}>
                       Delete
                     </button>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ) : (
-          <p>No Todos</p>
-        )}
+                    <button onClick={() => this.editTodo(todo.id, todo.text)}>
+                      Edit
+                    </button>
+                  </>
+                ))}
+              </ul>
+            </div>
+          ) : (
+            <p>No Todos</p>
+          )}
 
-        <input
-          type="text"
-          value={this.state.inputValue}
-          onChange={(event) =>
-            this.setState({ inputValue: event.target.value })
-          }
-        />
-        <button onClick={this.addTodo}>Add Todo</button>
+          <input
+            type="text"
+            value={this.state.inputValue}
+            onChange={(event) =>
+              this.setState({ inputValue: event.target.value })
+            }
+          />
+          <button onClick={this.addTodo}>Add Todo</button>
+          {this.state.editInput.edit && (
+            <div>
+              <h2>Edit yout list</h2>
+              <input
+                type="text"
+                value={this.state.editInput.inputValue}
+                onChange={(e) => {
+                  this.setState({
+                    editInput: {
+                      ...this.state.editInput,
+                      inputValue: e.target.value,
+                    },
+                  });
+                }}
+              />
+              <button onClick={this.addEdit}>Edit Item</button>
+            </div>
+          )}
+        </div>
       </div>
     );
   }
